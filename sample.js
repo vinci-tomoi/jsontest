61a4a5f43ea83f7965b71349f20709bc389dc2b4
vinci-tomoi <kanako.tomoi@i-vinci.co.jp>
2020-01-16 14:16:13 +0900

    Add: 入力データをローカルに保存する機能

--- sample.js
+++ sample.js
@@ -1,5 +1,29 @@
-var form = $('form');
-var formData = form.serializeArray();
-var formJson = JSON.stringify(formData);
+$(function() {
+  
+  $('#save').click(function() {
+    var name = document.getElementById("name").value;
+    setBlobUrl("download", name);
+  });
 
-localStorage.setItem('form_data', formJson);
+  const setBlobUrl = (id, content) => {
+
+    var blob = new Blob([ content ], { "type" : "application/x-msdownload" });
+
+    window.URL = window.URL || window.webkitURL;
+    $("#" + id).attr("href", window.URL.createObjectURL(blob));
+    $("#" + id).attr("download", "tmp.txt");
+
+  };
+
+
+});
+
+
+// $('save').click(() => {
+//   var form = $('form');
+//   var formData = form.serializeArray();
+//   var formJson = JSON.stringify(formData);
+  
+//   localStorage.setItem('form_data', formJson);
+// });
+
